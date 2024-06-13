f6dbd01de60fba0bdef2e0e92658792c893a63ec
Grant Patterson <grantpa@umich.edu>
2024-06-13 01:24:37 -0400

    Runtime: 82 ms (5.17%), Memory: 16.5 MB (90.14%) - Gitcode

--- 9-palindrome-number/9. Palindrome Number.py
+++ 9-palindrome-number/9. Palindrome Number.py
@@ -1,5 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
         rev = ""
         for n in range(1,len(str(x))+1):
             rev += str(x)[len(str(x))-n]
