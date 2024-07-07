addd715aee0c10e88f5f6381e4a658ee1302c459
Grant Patterson <grantpa@umich.edu>
2024-07-07 16:40:10 -0400

    Runtime: 18 ms (99.99%), Memory: 16.5 MB (28.24%) - Gitcode

--- 21-merge-two-sorted-lists/21. Merge Two Sorted Lists.py
+++ 21-merge-two-sorted-lists/21. Merge Two Sorted Lists.py
@@ -6,8 +6,9 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
+        #dont have to worry about edgecase of inserting into empty list
         tail = dummy
-
+        #while they are non-null
         while list1 and list2:
             if list1.val < list2.val:
                 tail.next = list1
@@ -16,9 +17,12 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
+        #what if one is empty and other isn't right now
         if list1:
             tail.next = list1
+            #inserts remaining portion of l1
         elif list2:
             tail.next = list2
-        
+
+        #return dummy.next and it returns the whole list as the pointers go through it
         return dummy.next
