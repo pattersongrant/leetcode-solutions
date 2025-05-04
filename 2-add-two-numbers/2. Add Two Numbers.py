8565078bc7ec85dd499eb04db9d91178551de094
Grant Patterson <grantpa@umich.edu>
2025-05-04 14:23:36 -0400

    Runtime: 11 ms (10.39%), Memory: 17.8 MB (84.91%) - Gitcode

--- 2-add-two-numbers/2. Add Two Numbers.py
+++ 2-add-two-numbers/2. Add Two Numbers.py
@@ -6,34 +6,28 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        prev, cur = None, l1
+        cur = l1
         
         first = ""
         second = ""
 
         while cur:
             first += str(cur.val)
-            prev = cur
             cur = cur.next
 
-        prev, cur = None, l2
+        cur = l2
 
         while cur:
             second += str(cur.val)
-            prev = cur
             cur = cur.next
 
         ans = int(first[::-1]) + int(second[::-1]) #add together actual numbers
         ans = str(ans)[::-1] #back to reverse
 
-        prev = None
         cur = ListNode()
         head = cur
-        for i, n in enumerate(ans):
-            prev = cur
-            
+        for i, n in enumerate(ans):            
             cur.val = int(n)
-            
             if i != len(ans) - 1:
                 new_node = ListNode()
                 cur.next = new_node
