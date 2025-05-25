2e4faaa35de46c7ca5be322411301225d269e179
Grant Patterson <grantpa@umich.edu>
2025-05-25 15:17:19 -0400

    Runtime: 3 ms (73.52%), Memory: 17.9 MB (62.59%) - Gitcode

--- 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
+++ 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
@@ -3,6 +3,6 @@ class Solution:
         cost.append(0)
 
         for i in range(len(cost) - 3, -1 , -1):
-            cost[i] += min(cost[i+1], cost[i+2])
+            cost[i] += min(cost[i+1], cost[i+2]) #set value equal to distance to end
 
         return min(cost[0], cost[1])
