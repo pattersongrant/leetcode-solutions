3209118638ae8d64997e8e4f740a725b4834d045
Grant Patterson <grantpa@umich.edu>
2025-08-12 00:08:19 -0400

    Runtime: 3 ms (98.35%), Memory: 18.2 MB (79.84%) - Gitcode

--- 152-maximum-product-subarray/152. Maximum Product Subarray.py
+++ 152-maximum-product-subarray/152. Maximum Product Subarray.py
@@ -3,6 +3,13 @@ class Solution:
         res = -10
         curMin, curMax = 1, 1
 
+        total = 1
+        for n in nums:
+            total *= n
+        if total > 0:
+            return total
+
+
         for n in nums:
             tmp = curMax * n
             curMax = max(curMax * n, curMin * n, n)
