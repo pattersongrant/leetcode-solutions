4fadea8406cfe5158c3840390f40fe2220a10423
Grant Patterson <grantpa@umich.edu>
2025-08-12 22:50:19 -0400

    Runtime: 25 ms (14.33%), Memory: 18.1 MB (5.76%) - Gitcode

--- 326-power-of-three/326. Power of Three.py
+++ 326-power-of-three/326. Power of Three.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        for i in range(0, n):
+        for i in range(0, int(n/2)+1):
             if 3**i > n:
                 break
             if 3**i == n:
