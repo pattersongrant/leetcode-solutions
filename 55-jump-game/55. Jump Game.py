3ba7d7b62bfdea350873001ea02793f2d276cdb9
Grant Patterson <grantpa@umich.edu>
2025-08-21 16:45:51 -0400

    Runtime: 17 ms (69.87%), Memory: 18.8 MB (25.74%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -5,10 +5,13 @@ class Solution:
         if len(nums) == 1:
             return True
         for i in range(len(nums)-1):
+
             if i + nums[i] > maxIndex:
                 maxIndex = i + nums[i]
-            if nums[i] == 0:
-                if not maxIndex >= i+1:
-                    return False
+            if i+1 > maxIndex:
+                return False
+            # if nums[i] == 0:
+            #     if not maxIndex >= i+1:
+            #         return False
 
         return True
