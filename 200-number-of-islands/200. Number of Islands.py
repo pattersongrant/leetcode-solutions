b5f0737ca3002ebe40fcefb3f000da8b6b13f8ce
Grant Patterson <grantpa@umich.edu>
2025-10-19 01:59:24 -0400

    Runtime: 290 ms (15.98%), Memory: 20.3 MB (42.80%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -2,7 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
         rows = len(grid)
         cols = len(grid[0])
 
@@ -14,7 +14,6 @@ class Solution:
             grid[r][c] = "0"
 
             while q:
-                directions = [(1,0), (-1,0), (0,1), (0,-1)]
                 r1, c1 = q.popleft()
                 
                 for x, y in directions:
