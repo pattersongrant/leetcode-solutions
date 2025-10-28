e877a207f17f866e1060d1edf94d5f0b0d91932b
Grant Patterson <grantpa@umich.edu>
2025-10-28 13:31:26 -0400

    Runtime: 9 ms (30.61%), Memory: 21.5 MB (34.86%) - Gitcode

--- 155-min-stack/155. Min Stack.py
+++ 155-min-stack/155. Min Stack.py
@@ -6,26 +6,18 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if self.minStack == [] or val <= self.minStack[-1]:
+        if len(self.minStack) == 0 or self.minStack[-1] >= val:
             self.minStack.append(val)
+        
 
     def pop(self) -> None:
-        a = self.stack.pop()
-        if self.minStack != [] and self.minStack[-1] == a:
+        if self.stack[-1] == self.minStack[-1]:
             self.minStack.pop()
+        self.stack.pop()
+        
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
         return self.minStack[-1]
-
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
