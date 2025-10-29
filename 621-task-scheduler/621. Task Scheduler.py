19649563bae447c9a1a7c4802fe1f7b8e7c08a1f
Grant Patterson <grantpa@umich.edu>
2025-10-29 01:23:40 -0400

    Runtime: 125 ms (55.42%), Memory: 19.1 MB (90.17%) - Gitcode

---
+++ 621-task-scheduler/621. Task Scheduler.py
@@ -0,0 +1,26 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+    
+
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        q = deque()
+        time = 0
+
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                # cnt = 1 + heapq.heappop(maxHeap)
+                # if cnt:
+                #     q.append([cnt, time + n])
+                if maxHeap[0] != -1:
+                    q.append([maxHeap[0]+1, time + n])
+                heapq.heappop(maxHeap)
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+
+        return time
+
+        
