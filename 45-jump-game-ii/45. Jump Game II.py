ae306a7b06fd2b999e9312e2e9781a19b658bad5
Grant Patterson <grantpa@umich.edu>
2025-10-31 18:40:00 -0400

    Runtime: 3 ms (97.04%), Memory: 18.5 MB (74.77%) - Gitcode

--- 45-jump-game-ii/45. Jump Game II.py
+++ 45-jump-game-ii/45. Jump Game II.py
@@ -1,23 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        furthestNext = 0
-        bestNextIdx = 0
-        curJumpIdx = 0
-        curJumpDistance = nums[0]
-        jumps = 0
+        res = 0
+        l = r = 0
 
-        if len(nums) == 1:
-            return 0
-
-        for i in range(len(nums)):
-            furthestNext = max(i+nums[i], furthestNext)
-            if furthestNext == i+nums[i]:
-                bestNextIdx = i
-            if i == len(nums) - 1:
-                return jumps + 1
-            if i+1 > curJumpDistance + curJumpIdx:
-                curJumpIdx = bestNextIdx
-                curJumpDistance = nums[bestNextIdx]
-                jumps += 1
-                if curJumpDistance + curJumpIdx >= len(nums) - 1:
-                    return jumps + 1
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r+1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
