c2eafcb177ab9ac586f6277139e75aa2b20c5c18
Grant Patterson <grantpa@umich.edu>
2025-11-01 18:01:05 -0400

    Runtime: 3 ms (19.95%), Memory: 18.4 MB (86.83%) - Gitcode

---
+++ 102-binary-tree-level-order-traversal/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = deque()
+
+        q.append(root)
+        res = []
+
+        while q:
+            level = []
+            curLen = len(q)
+            for i in range(curLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left: 
+                        q.append(node.left)
+                    if node.right: 
+                        q.append(node.right)
+            if level:
+                res.append(level)
+
+        return res
+
+
