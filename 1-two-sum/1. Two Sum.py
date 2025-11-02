3dc2af828140f82f74e8a79d0f6436b2fe4108c9
Grant Patterson <grantpa@umich.edu>
2025-11-02 15:57:01 -0500

    Runtime: 0 ms (100.00%), Memory: 18.9 MB (51.25%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,17 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-
-
+        
+        hashmap = {} #number : index
+    
         for i, n in enumerate(nums):
-            if target - n in hashmap:
+            
+            if target-n in hashmap:
                 return [i, hashmap[target-n]]
-            hashmap[n] = i
-
+            
+            if n not in hashmap:
+                hashmap[n] = i
         
 
-
-
-
+            
+            
+                
 
 
