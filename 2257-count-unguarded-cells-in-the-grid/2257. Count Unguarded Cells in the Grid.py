b649ea256748fb5e235e6b2b5032639fbb001038
Grant Patterson <grantpa@umich.edu>
2025-11-02 15:13:59 -0500

    Runtime: 555 ms (32.82%), Memory: 45.5 MB (19.70%) - Gitcode

--- 2257-count-unguarded-cells-in-the-grid/2257. Count Unguarded Cells in the Grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257. Count Unguarded Cells in the Grid.py
@@ -4,8 +4,6 @@ class Solution:
 
         graph = []
 
-        visitedByGuard = set()
-
         for i in range(ROWS):
             graph.append(["E"] * COLS)
 
@@ -15,20 +13,23 @@ class Solution:
         for [r,c] in walls:
             graph[r][c] = "W"
 
-        def dfs(r, c, vert, curGuard):
+        def dfs(r, c, vert, prev):
             if (r < 0 or c < 0 or r == ROWS or c == COLS 
-                or graph[r][c] == "W" or graph[r][c] == "G" or 
-                (r,c,curGuard) in visitedByGuard):
+                or graph[r][c] == "W" or graph[r][c] == "G"):
                 return
 
             graph[r][c] = "O"
-            visitedByGuard.add((r,c,curGuard))
+
             if vert:
-                dfs(r+1, c, True, curGuard)
-                dfs(r-1, c, True, curGuard)
+                if prev != (r+1, c):
+                    dfs(r+1, c, True, (r,c))
+                if prev != (r-1, c):
+                    dfs(r-1, c, True, (r,c))
             if not vert:
-                dfs(r, c+1, False, curGuard)
-                dfs(r, c-1, False, curGuard)
+                if prev != (r, c+1):
+                    dfs(r, c+1, False, (r,c))
+                if prev != (r, c-1):    
+                    dfs(r, c-1, False, (r,c))
 
 
         for r in range(ROWS):
@@ -44,5 +45,5 @@ class Solution:
             for c in range(COLS):
                 if graph[r][c] == "E":
                     res += 1
-                    
+
         return res
