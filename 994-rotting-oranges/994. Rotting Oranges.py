f3f16c9d8b57debde0561e4aa3c092425ffba53e
Grant Patterson <grantpa@umich.edu>
2025-11-01 22:36:34 -0400

    Runtime: 2 ms (85.98%), Memory: 17.9 MB (44.25%) - Gitcode

--- 994-rotting-oranges/994. Rotting Oranges.py
+++ 994-rotting-oranges/994. Rotting Oranges.py
@@ -1,52 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        minute = 0
-        self.infected = 0
+        time, fresh = 0, 0
         q = deque()
-        visited = set()
-        self.startFresh = 0
-
-        def addQueue(r, c):
-            if (r < 0 or r >= ROWS or c < 0 or c >= COLS or 
-                (r,c) in visited or grid[r][c] == 0):
-                return
-            visited.add((r, c))
-            self.infected += 1
-            grid[r][c] = 2
-            q.append((r,c)) 
-
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     q.append((r,c))
-                    visited.add((r, c))
                 if grid[r][c] == 1:
-                    self.startFresh += 1
+                    fresh += 1
 
-        if self.startFresh == 0:
-            return 0
 
-        if len(q) == 0:
-            return -1
-
-        while q:
-            oldInfect = self.infected
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        while q and fresh > 0:
             for i in range(len(q)):
                 r, c = q.popleft()
-                addQueue(r+1, c)
-                addQueue(r-1, c)
-                addQueue(r, c+1)
-                addQueue(r, c-1)
-            minute += 1
-            if self.infected == self.startFresh:
-                return minute
-            if oldInfect == self.infected:
-                    return -1
-        if self.infected != self.startFresh:
-                return -1
-        return minute
+                
+                for dr, dc in directions:
+                    row, col= r + dr, c+ dc
+                    if (row < 0 or row >= ROWS or col < 0 or col >= COLS or grid[row][col] != 1):
+                        continue
+                    grid[row][col] = 2
+                    fresh -= 1
+                    q.append((row,col)) 
+            time += 1
+            
+        if fresh != 0:
+            return -1
+        return time
     
             
 
