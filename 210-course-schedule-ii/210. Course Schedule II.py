ba46bf9903cd4de4192e72be1434a643973334a5
Grant Patterson <grantpa@umich.edu>
2025-11-05 16:43:59 -0500

    Runtime: 5 ms (36.00%), Memory: 19.8 MB (20.95%) - Gitcode

---
+++ 210-course-schedule-ii/210. Course Schedule II.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        crsToPre = {i:[] for i in range(numCourses)}
+        res = []
+        taken = set()
+        for a, b in prerequisites:
+            crsToPre[a].append(b)
+
+        visit = set()
+        def dfs(crs):
+            if crsToPre[crs] == []:
+                if crs not in taken:
+                    res.append(crs)
+                    taken.add(crs)
+                return True
+
+            
+            if crs in visit:
+                return False
+            
+            visit.add(crs)
+            for pre in crsToPre[crs]:
+                if not dfs(pre): return False
+                if pre not in taken:
+                    res.append(pre)
+                    taken.add(pre)
+                
+
+            crsToPre[crs] = []
+            visit.remove(crs)
+            if crs not in taken:
+                res.append(crs)
+                taken.add(crs)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return res
+            
+            
+            
