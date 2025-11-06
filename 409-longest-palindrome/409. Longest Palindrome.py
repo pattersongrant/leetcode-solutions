c1a290121d41869aeca53d1e3ad07a5f8177493c
Grant Patterson <grantpa@umich.edu>
2025-11-06 00:58:33 -0500

    Runtime: 3 ms (40.56%), Memory: 17.6 MB (94.93%) - Gitcode

--- 409-longest-palindrome/409. Longest Palindrome.py
+++ 409-longest-palindrome/409. Longest Palindrome.py
@@ -1,32 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        hashmap = Counter(s)
+        count = Counter(s)
         res = 0
-        maxOdd = [0]
-        for letter in hashmap:
-            n = hashmap[letter]
-
-            if n % 2 != 0:
-                if maxOdd and n > maxOdd[-1]:
-                    maxOdd.append(n)
-                    continue
-
-            if n < 2:
-                continue
-
-            if n % 2 != 0:
-                res += (n-1)
-                continue
-            res += n
-
-        if maxOdd:
-            res += maxOdd[-1]
-            maxOdd.pop()
-
-            for n in maxOdd:
-                if n < 2:
-                    continue
-                res += (n-1)
-        return res
-
+        odd = False
+        for l in count:
+            if count[l] % 2 == 1:
+                odd = True
+            if count[l] >= 2:
+                res += count[l] - (count[l] % 2)
+  
+            
+        return res if not odd else res + 1
             
