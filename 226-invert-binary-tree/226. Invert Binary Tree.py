ed2b874a7add180eecb2cc683f5c8d0d14846e66
Grant Patterson <grantpa@umich.edu>
2025-11-08 09:56:17 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (50.07%) - Gitcode

---
+++ 226-invert-binary-tree/226. Invert Binary Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def dfs(node):
+            if not node:
+                return
+            old_right = node.right
+            node.right = node.left
+            node.left = old_right
+
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+
+        return root
+
+
+
