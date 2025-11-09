6af3ef32b3fad7e5f9aa594c0d817be9faa00619
Grant Patterson <grantpa@umich.edu>
2025-11-09 10:47:15 -0500

    Runtime: 233 ms (84.90%), Memory: 20 MB (82.28%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,38 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        rows = len(grid)
-        cols = len(grid[0])
+        ROWS, COLS = len(grid), len(grid[0])
 
-        islands = 0
+        self.res = 0
+
+        q = deque()
+
+        def addQueue(r, c):
+            if (r >= 0 and r < ROWS and c >= 0 and c < COLS 
+                and grid[r][c] == "1"):
+                grid[r][c] = "0"
+                q.append((r,c))
 
         def bfs(r, c):
-            q = collections.deque()
-            q.append((r, c))
-            grid[r][c] = "0"
+            self.res += 1
+            q.append((r,c))
 
             while q:
-                r1, c1 = q.popleft()
-                
-                for x, y in directions:
+                r, c = q.popleft()
+                grid[r][c] = "0"
 
-                    if ((r1+x >= 0 and r1+x < rows) and 
-                        (c1+y >= 0 and c1+y < cols) and 
-                        grid[r1+x][c1+y] == "1"):
-                        q.append((r1+x, c1+y))
-                        grid[r1+x][c1+y] = "0"
-                        
-            
+                addQueue(r+1, c)
+                addQueue(r-1, c)
+                addQueue(r, c+1)
+                addQueue(r, c-1)
 
 
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == "1":
-                    bfs(r, c)
-                    islands += 1
+                    bfs(r,c)
 
+        return self.res
 
-        return islands
 
