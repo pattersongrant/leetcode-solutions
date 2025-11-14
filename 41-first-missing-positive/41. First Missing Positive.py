57fa2341e5e4340afad9ffabde4e2e0f8ede21d8
Grant Patterson <grantpa@umich.edu>
2025-11-14 12:23:22 -0500

    Runtime: 87 ms (5.98%), Memory: 29.1 MB (38.96%) - Gitcode

--- 41-first-missing-positive/41. First Missing Positive.py
+++ 41-first-missing-positive/41. First Missing Positive.py
@@ -5,21 +5,23 @@ class Solution:
         #i: 0 1  2 3 4
         #n: 2 0  4 1 0
 
+        length = len(nums)
+
         #first loop: set all 0's and negatives and too big vals to len()
         for i in range(len(nums)):
-            if nums[i] <= 0 or nums[i] > len(nums):
-                nums[i] = abs(len(nums))+1
+            if nums[i] <= 0 or nums[i] > length:
+                nums[i] = abs(length)+1
                 continue
 
         #second loop: set all nums[n-1] to negative
-        for i in range(len(nums)):
-            if nums[i] == len(nums)+1 or nums[i] == -(len(nums)+1): 
+        for i in range(length):
+            if nums[i] == length+1 or nums[i] == -(length+1): 
                 continue
             nums[abs(nums[i])-1] = abs(nums[abs(nums[i])-1]) * -1
 
 
         #third loop check range of possible and return first that index - 1 is positive
-        for i in range(1, len(nums)+1):
+        for i in range(1, length+1):
             if nums[i-1] > 0:
                 return i
 
