84d1a0b14f589e781d6de5b2a5af9f818121f117
Grant Patterson <grantpa@umich.edu>
2025-11-14 12:17:02 -0500

    Runtime: 95 ms (5.13%), Memory: 28.9 MB (76.23%) - Gitcode

--- 41-first-missing-positive/41. First Missing Positive.py
+++ 41-first-missing-positive/41. First Missing Positive.py
@@ -1,11 +1,27 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        
+        #must be able to use O(1) space.
 
-        nums = set(nums)
+        #i: 0 1  2 3 4
+        #n: 2 0  4 1 0
 
+        #first loop: set all 0's and negatives and too big vals to len()
+        for i in range(len(nums)):
+            if nums[i] <= 0 or nums[i] > len(nums):
+                nums[i] = abs(len(nums))+1
+
+        #second loop: set all nums[n-1] to negative
+        for n in nums:
+            if n == len(nums)+1 or n == -(len(nums)+1): 
+                continue
+            nums[abs(n)-1] = abs(nums[abs(n)-1]) * -1
+
+
+        #third loop check range of possible and return first that index - 1 is positive
         for i in range(1, len(nums)+1):
-            if i not in nums:
+            if nums[i-1] > 0:
                 return i
+
+        #else return len + 1
 
         return len(nums) + 1
