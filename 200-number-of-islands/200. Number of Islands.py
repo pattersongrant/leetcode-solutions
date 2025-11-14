eeb164a8812afddefdb7a12e89a97b3b4fa15d13
Grant Patterson <grantpa@umich.edu>
2025-11-14 13:04:25 -0500

    Runtime: 240 ms (70.22%), Memory: 20.1 MB (68.09%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,19 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        
-        res = 0
 
         q = deque()
+        islands = 0
 
         def addQueue(r, c):
-            if (r >= 0 and r < ROWS and c >= 0 and c < COLS 
-                and grid[r][c] == "1"):
+            if (r >= 0 and r != ROWS and c >= 0 and 
+                c != COLS and grid[r][c] == "1"):
+                q.append((r, c))
                 grid[r][c] = "0"
-                q.append((r,c))
 
         def bfs(r, c):
-            q.append((r,c))
+            q.append((r, c))
 
             while q:
                 r, c = q.popleft()
@@ -22,14 +21,15 @@ class Solution:
                 addQueue(r, c+1)
                 addQueue(r, c-1)
 
-
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    grid[r][c] = "0"
-                    bfs(r,c)
-                    res += 1
-
-        return res
+                    bfs(r, c)
+                    islands += 1
 
 
+        return islands
+
+
+
+        
