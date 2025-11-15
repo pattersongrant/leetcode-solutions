46b4c25d08eaaa4ccba619160de54400499205a1
Grant Patterson <grantpa@umich.edu>
2025-11-14 23:59:03 -0500

    Runtime: 7 ms (89.59%), Memory: 17.8 MB (86.15%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,20 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(t) != len(s):
-            return False
-
-        hashmap_s = Counter(s)
-        hashmap_t = Counter(t)
-
-        if len(hashmap_s) != len(hashmap_t):
-            return False
-
-        for key in hashmap_s:
-            if key not in hashmap_t:
-                return False
-            if hashmap_s[key] != hashmap_t[key]:
-                return False
-        
-        return True
-
-            
+        return Counter(s) == Counter(t)
