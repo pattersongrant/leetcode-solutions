358ebb2891186511f4570df63362cea36f9e611f
Grant Patterson <grantpa@umich.edu>
2025-11-15 14:38:45 -0500

    Runtime: 7 ms (50.18%), Memory: 24.7 MB (7.79%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,36 +1,29 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        countsToElements = defaultdict(list)
+        count = {}
 
-        nums.sort()
-        cur = 1
-        maxCount = 1
-        for i in range(len(nums)):
+        bucket = []
 
-            if i > 0 and nums[i] == nums[i - 1]:
-                cur += 1
-            elif i > 0 and nums[i] != nums[i-1]:
-                countsToElements[cur].append(nums[i-1])
-                maxCount = max(maxCount, cur)
-                cur = 1
-        countsToElements[cur].append(nums[-1])
-        maxCount = max(maxCount, cur)
+        for i in range(len(nums) + 1):
+            bucket.append([])
+
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+
+        for val in count:
+            count_val = count[val]
+            print(count_val)
+            if bucket[count_val] == None:
+                bucket[count_val] = [val]
+            else:
+                bucket[count_val].append(val)
+
         res = []
-        count = 0
+        for i in range(len(bucket) - 1, -1, -1):
+            if len(res) >= k: break
+            for num in bucket[i]:
+                if len(res) >= k: break
+                res.append(num)
 
-        for i in range(maxCount, -1, -1):
-            if i in countsToElements:
-                for elt in countsToElements[i]:
-                    if count >= k: break
-                    res.append(elt)
-                    count += 1
-
-        return res        
-
-
-
-            
-
-
-
+        return res
+        
