0ec816baa69815d1412eaa7294f798feefe1fcbd
Grant Patterson <grantpa@umich.edu>
2025-11-15 13:22:49 -0500

    Runtime: 15 ms (49.96%), Memory: 22.7 MB (11.50%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,18 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        countsToLists = {}
+        countsToLists = defaultdict(list)
 
         for s in strs:
             count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')] += 1
-            
-            count = tuple(count)
-            if count in countsToLists:
-                countsToLists[count].append(s)
-            else:
-                countsToLists[count] = [s]
+                
+            countsToLists[tuple(count)].append(s)
 
         res = []
         for ct in countsToLists:
