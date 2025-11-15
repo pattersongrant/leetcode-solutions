91acbbbb69fd2634a9125d0422e6944b7ec3bab5
Grant Patterson <grantpa@umich.edu>
2025-11-15 18:02:28 -0500

    Runtime: 10294 ms (5.18%), Memory: 21.2 MB (6.81%) - Gitcode

--- 543-diameter-of-binary-tree/543. Diameter of Binary Tree.py
+++ 543-diameter-of-binary-tree/543. Diameter of Binary Tree.py
@@ -4,28 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:    
-        self.res = 0
-
-        #returns the height
-        def dfs(cur):
-            if cur == None:
-                return 0
-
-            left = dfs(cur.left)
-            right = dfs(cur.right)
-
-            self.res = max(self.res, left+right)
-
-            return 1 + max(left, right)
-            
-        dfs(root)
-        return self.res
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        maxAtNode = self.maxDepth(root.left) + self.maxDepth(root.right)
 
-
-
+        return max(maxAtNode, self.diameterOfBinaryTree(root.left), self.      diameterOfBinaryTree(root.right))
+        
         
