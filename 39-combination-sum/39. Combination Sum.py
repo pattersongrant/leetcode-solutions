7aba3628d08d94a8db0233d4476b4b99fab4127f
Grant Patterson <grantpa@umich.edu>
2025-11-16 14:19:56 -0500

    Runtime: 11 ms (57.41%), Memory: 17.9 MB (60.67%) - Gitcode

--- 39-combination-sum/39. Combination Sum.py
+++ 39-combination-sum/39. Combination Sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        #3 choices - include and go next, include and add again, skip
+        #2 choices - include (but don't increment) OR go to next elt
         res = []
 
         cur = []
