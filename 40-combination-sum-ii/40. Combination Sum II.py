3e021236a9805bee4efb2a4a3ae0a858b8db23a3
Grant Patterson <grantpa@umich.edu>
2025-11-16 15:20:14 -0500

    Runtime: 29 ms (26.92%), Memory: 18 MB (35.29%) - Gitcode

--- 40-combination-sum-ii/40. Combination Sum II.py
+++ 40-combination-sum-ii/40. Combination Sum II.py
@@ -1,26 +1,33 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        candidates.sort() #SORT so you can easily increment through dupe elements
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
+
+        cur = []
+
+        candidates.sort()
+
+        def dfs(i, curSum):
+            if i >= len(candidates) or curSum >= target:
+                if curSum == target:
+                    res.append(cur.copy())
                 return
 
-            if total > target or i >= len(candidates) or candidates[i] > target:
-                return
-
-            #include candidates[i]
             cur.append(candidates[i])
-            dfs(i+1, cur, total + candidates[i])
+            dfs(i+1, curSum + candidates[i])
+
             cur.pop()
 
-            #whiel i+1 in bounds...
-            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
-                i = i+1
+            while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i+= 1
 
-            #skip candidates[i]
-            dfs(i+1, cur, total)
-                         
-        dfs(0, [], 0)
+            dfs(i+1, curSum)
+                
+            
+            
+
+                
+
+
+        dfs(0, 0)
         return res
+
