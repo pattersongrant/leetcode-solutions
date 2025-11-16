8752731872bd1c2b1adbaa31a6796d959c671a0a
Grant Patterson <grantpa@umich.edu>
2025-11-16 13:21:30 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (90.42%) - Gitcode

--- 78-subsets/78. Subsets.py
+++ 78-subsets/78. Subsets.py
@@ -2,24 +2,19 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        subset = []
+        cur = []
+        #O(n * 2^n), you either include or dont include for each num
 
         def dfs(i):
             if i >= len(nums):
-                res.append(subset.copy()) #we've reached the leaf node. .copy() returns a copy of the subset as it will be modified.
+                res.append(cur.copy())
                 return
             
-            subset.append(nums[i]) #decision to include nums[i]
-            dfs(i + 1)
+            cur.append(nums[i])
+            dfs(i+1)
 
-
-            #decision NOT to include nums[i]
-            subset.pop()
-            dfs(i + 1)
+            cur.pop()
+            dfs(i+1)
 
         dfs(0)
-
-            
-
-
         return res
