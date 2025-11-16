7e6fcda45ebd4ebc40c7520c25d8e7b370d03215
Grant Patterson <grantpa@umich.edu>
2025-11-16 14:19:15 -0500

    Runtime: 8 ms (67.86%), Memory: 17.8 MB (60.67%) - Gitcode

--- 39-combination-sum/39. Combination Sum.py
+++ 39-combination-sum/39. Combination Sum.py
@@ -1,32 +1,26 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         #3 choices - include and go next, include and add again, skip
-
+        res = []
 
         cur = []
-        resSet = set()
+
 
         def dfs(i, curSum):
             if i >= len(nums) or curSum >= target:
                 if curSum == target:
-                    resSet.add(tuple(cur.copy()))
+                    res.append(cur.copy())
                 return
 
 
             cur.append(nums[i])
             dfs(i, curSum + nums[i])
 
-            dfs(i+1, curSum + nums[i])
-
             cur.pop()
             dfs(i+1, curSum)
 
         dfs(0, 0)
 
-        res = []
-
-        for tu in resSet:
-            res.append(list(tu))
         return res
             
             
