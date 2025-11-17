3e9a5e5ec112f46cab75b42919dfbcbbce1f1984
Grant Patterson <grantpa@umich.edu>
2025-11-16 22:05:38 -0500

    Runtime: 5 ms (6.90%), Memory: 18.1 MB (44.66%) - Gitcode

--- 22-generate-parentheses/22. Generate Parentheses.py
+++ 22-generate-parentheses/22. Generate Parentheses.py
@@ -2,28 +2,26 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
 
-        cur = ""
+
         maxChar = n * 2
 
-        def dfs(openp, closedp):
-            nonlocal cur
-            if len(cur) == n*2 and openp == closedp:
+        def dfs(cur, openp, closedp):
+            if len(cur) == maxChar and openp == closedp:
                 res.append(cur)
-
-            if len(cur) > n*2:
                 return
+
+            if len(cur) > n * 2:
+                return
+
             old = cur
 
-            if openp < n*2:    
-                cur += "("
-                dfs(openp + 1, closedp)
+            if openp < maxChar:    
+                dfs(cur + "(", openp + 1, closedp)
 
             if closedp < openp:
-                cur = old
-                cur += ")"
-                dfs(openp, closedp + 1)
+                dfs(cur + ")", openp, closedp + 1)
 
-        dfs(0, 0)
+        dfs("", 0, 0)
 
         return res
             
