48c6f1cfd7b0174a6e418500213f2c4e3fab4c94
Grant Patterson <grantpa@umich.edu>
2025-11-17 19:30:50 -0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (68.60%) - Gitcode

--- 91-decode-ways/91. Decode Ways.py
+++ 91-decode-ways/91. Decode Ways.py
@@ -1,27 +1,49 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = {len(s) : 1}
+        plus1, plus2 = 1, 0
 
 
-
-        def dfs(i): #returns number of ways starting from this index
-            if i in dp:
-                return dp[i]
+        for i in range(len(s) - 1, -1, -1):
 
             if s[i] == "0":
-                return 0
-            
-            res = dfs(i+1)
+                plus2 = plus1
+                plus1 = 0
+                continue
+
+            res = plus1
+            temp = plus2
+            plus2 = plus1
 
             if (i+1 < len(s) and ((s[i] == "1") or (s[i] == "2" and 
                 s[i+1] in "0123456"))):
-                res += dfs(i+2)
+
+                res += temp
+
+            
+            plus1 = res
+
+        return plus1
+
+
+
+        # def dfs(i): #returns number of ways starting from this index
+        #     if i in dp:
+        #         return dp[i]
+
+        #     if s[i] == "0":
+        #         return 0
+            
+        #     res = dfs(i+1)
+
+        #     if (i+1 < len(s) and ((s[i] == "1") or (s[i] == "2" and 
+        #         s[i+1] in "0123456"))):
+        #         res += dfs(i+2)
         
-            dp[i] = res
-            return res
+        #     dp[i] = res
+        #     return res
 
 
-        return dfs(0)
+        # return dfs(0)
         
 
             
