f8a5bc767a64a41a7ee62a1cfc58ba2991e55df0
Grant Patterson <grantpa@umich.edu>
2025-12-13 14:20:57 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (45.35%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -1,30 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        #rr:
-        #topdown
-        # #ways to get to up + 1 plus # ways to get from left + 1
-        #base case: 0 is 1
         ROWS, COLS = m, n
         
         memo = []
 
         for i in range(m):
-            memo.append([0 for i in range(n)])
+            memo.append([-1 for i in range(n)])
         memo[0][0] = 1
-        for r in range(ROWS):
-            for c in range(COLS):
-                if r == 0 and c == 0:
-                    continue
-                #add left and up
-                res = 0
-                if r - 1 >= 0:
-                    res += (memo[r-1][c])
-                if c - 1 >= 0:
-                    res += (memo[r][c-1])
-                
-                memo[r][c] = res
 
+        def numWays(r, c):
+            if memo[r][c] != -1:
+                return memo[r][c]
+
+            res = 0
+            if r - 1 >= 0:
+                res += (numWays(r-1, c))
+            if c - 1 >= 0:
+                res += (numWays(r, c-1))
+            
+            memo[r][c] = res
+
+            return res
         
 
-        return memo[ROWS-1][COLS-1]
+        return numWays(ROWS-1, COLS-1)
 
