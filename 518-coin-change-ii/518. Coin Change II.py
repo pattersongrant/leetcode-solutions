99d797d014c2087a4b8677c0f94549cef7f4b298
Grant Patterson <grantpa@umich.edu>
2025-12-13 21:08:38 -0500

    Runtime: 1559 ms (14.59%), Memory: 368.4 MB (6.41%) - Gitcode

---
+++ 518-coin-change-ii/518. Coin Change II.py
@@ -0,0 +1,26 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+
+        memo = {} #(i, cur) : num ways
+        def dfs(i, cur):
+            if i >= len(coins) or cur > amount:
+                return 0
+            if (i, cur) in memo:
+                return memo[i, cur]
+
+
+            if cur == amount:
+                return 1
+
+            res = 0
+            res += dfs(i + 1, cur) #move forward
+            res += dfs(i, cur + coins[i]) #take
+            memo[i, cur] = res
+            return res
+
+        return dfs(0, 0)
+
+            
+
+            
