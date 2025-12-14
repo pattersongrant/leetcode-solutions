a427574ebd032c24da3e38a6fb82ae63c9d3e363
Grant Patterson <grantpa@umich.edu>
2025-12-14 10:16:53 -0500

    Runtime: 75 ms (99.65%), Memory: 19 MB (78.15%) - Gitcode

---
+++ 2390-removing-stars-from-a-string/2390. Removing Stars From a String.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        arr = []
+        for c in s:
+            if c == "*":
+                arr.pop()
+                continue
+            arr.append(c)
+            
+
+
+        return "".join(arr)
+
