f9c0d8fa82b9be48346bb802b326361e39b3dc29
Grant Patterson <grantpa@umich.edu>
2025-12-18 17:33:13 -0500

    Runtime: 214 ms (99.40%), Memory: 19.6 MB (99.96%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,40 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        rows = len(grid)
-        cols = len(grid[0])
-
-        visited = set()
-        islands = 0
-
-        def bfs(r, c):
-            q = collections.deque()
-            visited.add((r, c))
-            q.append((r, c))
-            grid[r][c] = "0"
-
-            while q:
-                directions = [(1,0), (-1,0), (0,1), (0,-1)]
-                r1, c1 = q.popleft()
-                
-                for x, y in directions:
-                    if r1+x not in range(rows) or c1+y not in range(cols) or grid[r1+x][c1+y] == "0" or (r1+x, c1+y) in visited:
-                        continue
-                    q.append((r1+x, c1+y))
-                    visited.add((r1+x, c1+y))
-                    grid[r1+x][c1+y] = "0"
-                        
+        ROWS, COLS = len(grid), len(grid[0])
+        self.res = 0
+        def dfs(r, c):
             
+            grid[r][c] = "0"
+            if r + 1 < ROWS and grid[r+1][c] == "1":
+                dfs(r+1, c)
+            if r - 1 >= 0 and grid[r-1][c] == "1":
+                dfs(r-1, c)
+            if c + 1 < COLS and grid[r][c+1] == "1":
+                dfs(r, c+1)
+            if c - 1 >= 0 and grid[r][c-1] == "1":
+                dfs(r, c-1)
 
 
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == "1":
-                    bfs(r, c)
-                    islands += 1
-
-
-        return islands
-
+                    self.res += 1
+                    dfs(r,c)
+        return self.res
