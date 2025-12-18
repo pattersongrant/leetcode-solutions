1f9810b71e6c9a248879ec09dfef9149a2921f1a
Grant Patterson <grantpa@umich.edu>
2025-12-18 17:33:06 -0500

    Runtime: 329 ms (5.29%), Memory: 25.5 MB (21.21%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -13,25 +13,25 @@ class Solution:
             q = collections.deque()
             visited.add((r, c))
             q.append((r, c))
+            grid[r][c] = "0"
 
             while q:
                 directions = [(1,0), (-1,0), (0,1), (0,-1)]
                 r1, c1 = q.popleft()
                 
                 for x, y in directions:
-                    if (r1+x in range(rows) and 
-                        c1+y in range(cols) and 
-                        grid[r1+x][c1+y] == "1" and
-                        (r1+x, c1+y) not in visited):
-                        q.append((r1+x, c1+y))
-                        visited.add((r1+x, c1+y))
+                    if r1+x not in range(rows) or c1+y not in range(cols) or grid[r1+x][c1+y] == "0" or (r1+x, c1+y) in visited:
+                        continue
+                    q.append((r1+x, c1+y))
+                    visited.add((r1+x, c1+y))
+                    grid[r1+x][c1+y] = "0"
                         
             
 
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
+                if grid[r][c] == "1":
                     bfs(r, c)
                     islands += 1
 
