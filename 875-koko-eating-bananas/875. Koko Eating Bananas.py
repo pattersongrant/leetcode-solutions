42df3971440db043d111d80c6c83d92605f8d70c
Grant Patterson <grantpa@umich.edu>
2025-12-18 00:23:42 -0500

    Runtime: 374 ms (5.03%), Memory: 18.8 MB (99.57%) - Gitcode

--- 875-koko-eating-bananas/875. Koko Eating Bananas.py
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.py
@@ -1,17 +1,42 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
-
-        while  l <= r:
-            k = (l+r) // 2
-            hours = 0
-            for p in piles:
-                hours += math.ceil(p/k)
-            if hours <= h:
-                res = min(res, k)
-                r = k-1
-            else:
-                l = k+1
-        return res
         
+        #run the algorithm (0 + pilesMax) // 2
+        #(it will never be more than pilesMax)
+        # if the time is greater than h, run again on lower 
+
+        # use binary search with variable x from 0 to h
+        # to run the algorithm until you find the value of x that the pile can be eaten in h hours
+
+
+        l, r = 1, max(piles)
+        res = -1
+        while l <= r:
+            time = 0
+            m = (l + r) // 2
+
+            for n in piles:
+                if n/m != n // m * 1.0:
+                    time += int((n // m) + 1.0)
+                else:
+                    time += n//m
+            if time <= h:
+                res = m
+                r = m - 1
+            elif time > h:
+                l = m + 1
+
+        
+        return res
+
+
+
+
+            
+
+
+
+
+
+
+
