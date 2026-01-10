56cc44e5a6be44be6ac142ceba1424984cb48f37
Grant Patterson <grantpa@umich.edu>
2026-01-10 18:02:36 -0500

    Runtime: 170 ms (98.79%), Memory: 20.2 MB (74.28%) - Gitcode

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/712. Minimum ASCII Delete Sum for Two Strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        n, m = len(s1), len(s2)
+
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(n):
+            for j in range(m):
+                if s1[i] == s2[j]:
+                    dp[i+1][j+1] = dp[i][j] + ord(s1[i]) * 2
+
+                else:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+        total_ascii = sum(ord(c) for c in s1) + sum(ord(c) for c in s2)
+        return total_ascii - dp[n][m]
+
+
+        
