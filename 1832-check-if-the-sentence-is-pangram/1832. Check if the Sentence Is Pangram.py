9b5df6e6a6b0a38733407057a31c2ef83958c3ea
Grant Patterson <grantpa@umich.edu>
2026-01-27 22:12:43 -0500

    Runtime: 0 ms (100.00%), Memory: 19.6 MB (5.69%) - Gitcode

--- 1832-check-if-the-sentence-is-pangram/1832. Check if the Sentence Is Pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832. Check if the Sentence Is Pangram.py
@@ -1,16 +1,10 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        s = "abcdefghijklmnopqrstuvwxyz"
-
-        counts = Counter(s)
-
+        s = set(["a","b","c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+        
         for c in sentence:
-            counts[c] -= 1
+            if c in s:
+                s.remove(c)
         
-
-        for count in counts:
-            if counts[count] == 1:
-                return False
-        
-        return True
+        return len(s) == 0
         
