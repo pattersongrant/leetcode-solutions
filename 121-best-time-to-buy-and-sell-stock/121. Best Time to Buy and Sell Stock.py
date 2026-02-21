1d58c241d7dea1921f266c1160b25bb7ff88ec26
Grant Patterson <grantpa@umich.edu>
2026-02-21 15:24:13 -0500

    Runtime: 51 ms (57.82%), Memory: 29.3 MB (6.86%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
-        minVal = prices[0]
-
-        for n in prices:
-            res = max(res, n-minVal)
-            minVal = min(minVal, n)
-
-        return res
+            res = 0
+            buy = prices[0]
+        
+            for cur in prices[1::]:
+                res = max(res, cur-buy)
+                buy = min(cur, buy)
+        
+            return res
