77e0db250afea106df632441631653da6c7ba170
Grant Patterson <grantpa@umich.edu>
2026-02-23 14:15:03 -0500

    Runtime: 650 ms (40.65%), Memory: 22.3 MB (63.04%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -1,28 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
         
+        nums.sort()
+
+        res = []
         for i in range(len(nums)):
-            if i-1 >= 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            print(i)
-
-            l, r = i+1, len(nums) - 1
-            target = -nums[i]
-
+            l = i + 1
+            r = len(nums) - 1
             while l < r:
-                if nums[l] + nums[r] < target:
-                    l += 1
-                elif nums[l] + nums[r] > target:
+                if nums[i] + nums[l] + nums[r] > 0:
                     r -= 1
+                
+                elif nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
+                
                 else:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l-1] and l<r:
-                        l+= 1
-
-                    
+                    while l < len(nums) and nums[l] == nums[l-1]:
+                        l += 1
+        
         return res
-                
+
+
+
         
