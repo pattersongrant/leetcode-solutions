384673397c20efa85602f28477a7bf415c2c4ba9
Grant Patterson <grantpa@umich.edu>
2026-02-23 14:41:43 -0500

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (56.84%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,17 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        
         stack = []
 
         for c in s:
-            if c in "([{":
+            if c == "(" or c == "{" or c == "[":
                 stack.append(c)
             else:
-                if not stack:
+                if len(stack) == 0:
                     return False
-                elif ((c == ")" and stack[-1] != "(") or 
-                    (c == "}" and stack[-1] != "{") or
-                    (c == "]" and stack[-1] != "[")):
-                    return False
-                else:
+                if c == ")" and stack[-1] == "(":
                     stack.pop()
+                elif c == "}" and stack[-1] == "{":
+                    stack.pop()
+                elif c == "]" and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+        
         return len(stack) == 0
