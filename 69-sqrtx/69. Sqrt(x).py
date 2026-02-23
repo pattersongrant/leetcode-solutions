38a124952eaa54212b78e4291f53a7279f6cff5c
Grant Patterson <grantpa@umich.edu>
2026-02-23 14:24:31 -0500

    Runtime: 1535 ms (7.33%), Memory: 19.3 MB (72.24%) - Gitcode

--- 69-sqrtx/69. Sqrt(x).py
+++ 69-sqrtx/69. Sqrt(x).py
@@ -2,9 +2,8 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 0: return 0
         if x == 1: return 1
-        if x == 2: return 1
         prev = 0
-        for i in range(x):
+        for i in range(x+1):
             squared = i*i
             if squared == x:
 
