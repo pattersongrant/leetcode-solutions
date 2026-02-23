459c61987ed2090259f489678b8d976b09b7b398
Grant Patterson <grantpa@umich.edu>
2026-02-23 14:38:40 -0500

    Runtime: 214 ms (99.50%), Memory: 21.7 MB (47.45%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,35 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
         ROWS, COLS = len(grid), len(grid[0])
 
-        q = deque()
-        islands = 0
+        res = 0
 
-        def addQueue(r, c):
-            if (r >= 0 and r != ROWS and c >= 0 and 
-                c != COLS and grid[r][c] == "1"):
-                q.append((r, c))
-                grid[r][c] = "0"
+        def dfs(r, c):
+            grid[r][c] = "0"
 
-        def bfs(r, c):
-            q.append((r, c))
+            if r - 1 >= 0 and grid[r-1][c] == "1":
+                dfs(r-1, c)
+            if c - 1 >= 0 and grid[r][c-1] == "1":
+                dfs(r, c-1)
+            if r + 1 < ROWS and grid[r+1][c] == "1":
+                dfs(r+1, c)
+            if c + 1 < COLS and grid[r][c+1] == "1":
+                dfs(r, c+1)
 
-            while q:
-                r, c = q.popleft()
-                addQueue(r+1, c)
-                addQueue(r-1, c)
-                addQueue(r, c+1)
-                addQueue(r, c-1)
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    bfs(r, c)
-                    islands += 1
+                    res += 1
+                    dfs(r, c)
+        
+        return res
 
-
-        return islands
-
-
-
+        
         
