e43195048559583ce5fce268d2ea16b4bee1d63e
Grant Patterson <grantpa@umich.edu>
2026-02-25 02:34:52 -0500

    Runtime: 23 ms (47.83%), Memory: 20.6 MB (17.61%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -1,17 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        maxIndex = nums[0]
+        
+        furthest = 0
 
-        if len(nums) == 1:
-            return True
-        for i in range(len(nums)-1):
-
-            if i + nums[i] > maxIndex:
-                maxIndex = i + nums[i]
-            if i+1 > maxIndex:
-                return False
-            # if nums[i] == 0:
-            #     if not maxIndex >= i+1:
-            #         return False
-
-        return True
+        for i, n in enumerate(nums):
+            if i <= furthest:
+                furthest = max(furthest, i+n)
+            
+        return furthest >= len(nums) - 1
+            
