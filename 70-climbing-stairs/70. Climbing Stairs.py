781e2a003c5e70f2e66b5bc6882de9a7024b695c
Grant Patterson <grantpa@umich.edu>
2026-02-26 01:41:28 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (83.98%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -2,9 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         one, two = 1, 1
 
-        for i in range(n - 2, -1, -1):
-            temp = two
-            two = one + two
-            one = temp
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
 
-        return two
+        return one
