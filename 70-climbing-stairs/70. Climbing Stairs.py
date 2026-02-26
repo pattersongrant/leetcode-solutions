a12983ffd0f6f5b4686e6278b1d8840baa91450c
Grant Patterson <grantpa@umich.edu>
2026-02-26 01:41:15 -0500

    Runtime: 0 ms (100.00%), Memory: 17.5 MB (84.31%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,19 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #recurrence relation:
-        # you can come from either 1 step or 2 steps ago
-        # W(n) = W(n-1) + W(n-2) Therefore it's the ways to get to two away and ways to get to one away
-
-
         one, two = 1, 1
 
-        for i in range(2, n+1):
-            temp = one
-            one = one + two
-            two = temp
-        
-        return one
+        for i in range(n - 2, -1, -1):
+            temp = two
+            two = one + two
+            one = temp
 
-
-
-
+        return two
