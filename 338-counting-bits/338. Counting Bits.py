273a2d12c5c7bdeddf1e68bcc2dc95b9307b4959
Grant Patterson <grantpa@umich.edu>
2026-02-28 12:44:46 -0500

    Runtime: 71 ms (7.12%), Memory: 20.4 MB (38.65%) - Gitcode

---
+++ 338-counting-bits/338. Counting Bits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        def num1Bits(n):
+            res = 0
+            for i in range(len(bin(n))):
+                if n%2 != 0:
+                    res += 1
+                n=n>>1
+            return res
+        res = []
+        for i in range(n+1):
+            res.append(num1Bits(i))
+        
+        return res
+
+        
