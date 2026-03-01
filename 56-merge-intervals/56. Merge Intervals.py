de2508dc7566c5bbf65ff2531f1426a7ef29095b
Grant Patterson <grantpa@umich.edu>
2026-03-01 13:24:16 -0500

    Runtime: 11 ms (25.68%), Memory: 22.7 MB (54.87%) - Gitcode

--- 56-merge-intervals/56. Merge Intervals.py
+++ 56-merge-intervals/56. Merge Intervals.py
@@ -3,20 +3,7 @@ class Solution:
         intervals.sort()
         merge = [100000000, -10000000]
         res = []
-
-        '''
-        [[1,3],[2,6],[8,10],[15,18]]
-
-        merge[0] = min (intervals[i][0], intervals[i-1], 0)
-
-        if i +1 overlaps w cur:
-            adjust start/end
-        if not merging and not:
-            append cur
-        if not:
-            append merged interval
-        '''
-
+        
         for i in range(len(intervals)):
             if i+1 < len(intervals) and (intervals[i+1][0] <= intervals[i][1] or intervals[i+1][0] <= merge[1]):
                 merge[0] = min(intervals[i][0], merge[0])
