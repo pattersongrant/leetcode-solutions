cdabf6d890078c1380dfc0b970f5c8b0aa9b4673
Grant Patterson <grantpa@umich.edu>
2026-03-01 13:23:12 -0500

    Runtime: 12 ms (12.50%), Memory: 22.4 MB (87.26%) - Gitcode

--- 56-merge-intervals/56. Merge Intervals.py
+++ 56-merge-intervals/56. Merge Intervals.py
@@ -1,18 +1,31 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        res = [intervals[0]]
+        merge = [100000000, -10000000]
+        res = []
 
+        '''
+        [[1,3],[2,6],[8,10],[15,18]]
 
-        for i, li in enumerate(intervals[1:]):
-            if li[0] <= res[-1][1]:
-                res[-1][0] = min(res[-1][0], li[0])
-                res[-1][1] = max(res[-1][1], li[1])
+        merge[0] = min (intervals[i][0], intervals[i-1], 0)
+
+        if i +1 overlaps w cur:
+            adjust start/end
+        if not merging and not:
+            append cur
+        if not:
+            append merged interval
+        '''
+
+        for i in range(len(intervals)):
+            if i+1 < len(intervals) and (intervals[i+1][0] <= intervals[i][1] or intervals[i+1][0] <= merge[1]):
+                merge[0] = min(intervals[i][0], merge[0])
+                merge[1] = max(intervals[i+1][1], intervals[i][1], merge[1])
+
+            elif merge == [100000000, -10000000]:
+                res.append(intervals[i])
             else:
-                res.append(li)
-                    
-
+                res.append(merge)
+                merge = [100000000, -10000000]
         return res
-
-                
-
+        
