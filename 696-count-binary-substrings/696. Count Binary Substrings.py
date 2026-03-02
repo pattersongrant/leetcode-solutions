0bceda0ea9bec2dcc98faa81486504d88dc41a19
Grant Patterson <grantpa@umich.edu>
2026-03-02 02:23:28 -0500

    Runtime: 75 ms (20.92%), Memory: 19.6 MB (88.16%) - Gitcode

--- 696-count-binary-substrings/696. Count Binary Substrings.py
+++ 696-count-binary-substrings/696. Count Binary Substrings.py
@@ -12,24 +12,17 @@ class Solution:
         for i in range(1, len(s)):
             
             if s[i] == "0" and s[i-1] == "0":
-
                 zero_count += 1
-                if one_count >= zero_count:
-                    res += 1
             elif s[i] == "0" and s[i-1] != "0":
+                res += min(one_count, zero_count)
                 zero_count = 1
-                if one_count >= zero_count:
-                    res += 1
             elif s[i] == "1" and s[i-1] == "1":
-
                 one_count += 1
-                if zero_count >= one_count:
-                    res += 1
             elif s[i] == "1" and s[i-1] != "1":
+                res += min(one_count, zero_count)
                 one_count = 1
-                if zero_count >= one_count:
-                    res += 1
-            print(i, zero_count, one_count, res)
+                
+        res += min(one_count, zero_count)
         
         return res
             
