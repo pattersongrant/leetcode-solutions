a090a043a7e3f86dffe43528a6cde2a5afecda0a
Grant Patterson <grantpa@umich.edu>
2026-03-02 01:44:31 -0500

    Runtime: 37 ms (38.99%), Memory: 21.7 MB (88.66%) - Gitcode

--- 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
@@ -1,20 +1,24 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixSums = []
-        counts = {}
+        
+
+        # [1, 5, 2, 3, -1, 6, -2, 6]. k = 4
+        #.[1, 6, 8, 11, 10, 16, 14, 20]
+
+        prefix = {0 : 1}
         cur = 0
         res = 0
-        counts[0] = 1
         for n in nums:
             cur += n
-            if cur-k in counts:
-                res += counts[cur-k]
-            prefixSums.append(cur)
-            if cur in counts:
-                counts[cur] += 1
-            else:
-                counts[cur] = 1
+            if (cur - k) in prefix:
+                res += prefix[cur - k]
+            prefix[cur] = prefix.get(cur, 0) + 1
         
         return res
+            
+
 
         
+        
+
+            
