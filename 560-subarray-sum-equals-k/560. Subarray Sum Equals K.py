eb3307e7067dfc18c1a0a6b397ffd46c7fd67570
Grant Patterson <grantpa@umich.edu>
2026-03-02 01:45:20 -0500

    Runtime: 39 ms (35.18%), Memory: 23.2 MB (15.38%) - Gitcode

--- 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
@@ -5,14 +5,14 @@ class Solution:
         # [1, 5, 2, 3, -1, 6, -2, 6]. k = 4
         #.[1, 6, 8, 11, 10, 16, 14, 20]
 
-        prefix = {0 : 1}
+        prefix = defaultdict(int)
+        prefix[0] = 1
         cur = 0
         res = 0
         for n in nums:
             cur += n
-            if (cur - k) in prefix:
-                res += prefix[cur - k]
-            prefix[cur] = prefix.get(cur, 0) + 1
+            res += prefix[cur - k]
+            prefix[cur] += 1
         
         return res
             
