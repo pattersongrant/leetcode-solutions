d34c4148a9da2781b37d94bdc5ffd68656636efc
Grant Patterson <grantpa@umich.edu>
2026-03-02 19:22:25 -0500

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (71.16%) - Gitcode

--- 33-search-in-rotated-sorted-array/33. Search in Rotated Sorted Array.py
+++ 33-search-in-rotated-sorted-array/33. Search in Rotated Sorted Array.py
@@ -1,26 +1,61 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        l, r = 0, len(nums) - 1
+        '''
+        [4,5,6,7,0,1,2]
 
-        while r >= l:
+        target = 4 -- return index of target or -1 if not there
+        [6,7,0,1,2,4,5] 0 towards left
+        [2,4,5,6,7,0,1] 0 towards right
+        [1,2,4,5,6,7,0] 0 at end
+        
+        l, r = 0, 0
+        m = (l+r) // 2
+
+        if m is greater than r, then that means that m is in the rotated part of the array.
+            if target is greater than m, then remove everything to the left of m
+            if target is less than m
+                if target greater than r, remove right side
+                if target is less than r, remove left
+
+        if m is less than r, then that is part of the 0-part of the array.
+            if target is greater than r, then remove everything to right of m
+            if target is less than r, then remove everything to left of m
+
+        if m == target or l == target or r == target then return it
+        '''
+
+        l,r = 0, len(nums) - 1
+
+        while l <= r:
             m = (l+r) // 2
+            if nums[l] == target:
+                return l
             if nums[m] == target:
                 return m
-            #if m is in the right side
-            if nums[l] <= nums[m]:
-                if nums [l] > target or target > nums[m]:
-                    
+            if nums[r] == target:
+                return r
+            
+            if nums[m] > nums[r]:
+                if target > nums[m]:
                     l = m + 1
                 else:
-                    
-                    r = m - 1
-
+                    if target > nums[r]:
+                        r = m - 1
+                    else:
+                        l = m + 1
             else:
-                if target < nums[m] or nums[r] < target:
+                if target > nums[r]:
                     r = m - 1
-                else:
+                elif target > nums[m]:
                     l = m + 1
+                else:
+                    r = m - 1
+        return - 1
 
 
-        return -1
+
+
+
+
+
