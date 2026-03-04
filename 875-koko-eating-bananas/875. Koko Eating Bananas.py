a7838815f9e6a4637b4faa3fd7a65e910a988f54
Grant Patterson <grantpa@umich.edu>
2026-03-04 03:34:51 -0500

    Runtime: 158 ms (85.51%), Memory: 19.1 MB (94.04%) - Gitcode

--- 875-koko-eating-bananas/875. Koko Eating Bananas.py
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.py
@@ -1,27 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
-
         res = r
 
-        while l <= r:
-            
-            m = (l+r) // 2
-            hoursUsed = 0
-            for n in piles:
-                # old = hoursUsed
-                if m >= n:
-                    hoursUsed += 1
-                else:
-                    hoursUsed += math.ceil(n/m)
-                # print(m, n, hoursUsed - old)
-            
-            if hoursUsed <= h:
-                r = m - 1
-                res = min(m, res)
-            elif hoursUsed > h:
-                l = m + 1
-
-
-        
+        while  l <= r:
+            k = (l+r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/k)
+            if hours <= h:
+                res = min(res, k)
+                r = k-1
+            else:
+                l = k+1
         return res
+        
