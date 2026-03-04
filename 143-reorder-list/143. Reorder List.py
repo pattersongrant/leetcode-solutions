993c1c9a939c92c86ef312aff242383e3d067d75
Grant Patterson <grantpa@umich.edu>
2026-03-04 15:47:10 -0500

    Runtime: 14 ms (5.39%), Memory: 28 MB (40.24%) - Gitcode

---
+++ 143-reorder-list/143. Reorder List.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        arr = []
+        cur = head
+        while cur:
+            arr.append(cur)
+            cur = cur.next
+
+        newHead = ListNode()
+        cur = newHead
+
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            print(l, r)
+            cur.next = arr[l]
+            cur = cur.next
+            cur.next = None
+            cur.next = arr[r]
+            cur = cur.next
+            cur.next = None
+            l += 1
+            r -= 1
+        
+        return newHead.next
