6f8198ffda1d68593f0ba479ee75971f76f212a8
Grant Patterson <grantpa@umich.edu>
2026-03-06 18:47:57 -0500

    Runtime: 1 ms (29.78%), Memory: 19.3 MB (72.49%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -1,27 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        '''
-        I am going to do a top-down DP solution starting from the finish line
-
-        the recurrence relation for the number of paths at any given point is the number of paths to get to the square above + the number of paths to get to the square to the left
-
-        So I will start from the finish and the by using a top down recursion function will work from the base case all the way to the start.
-
-        I will also use a memo the size of m * n to ensure that I cache any already calculated squares
-        '''
-        dp = [[-1] * n for i in range(m)]
+        #bottom up DP
+        dp = [[0] * n for i in range(m)]
         dp[0][0] = 1
+
+        for r in range(m):
+            for c in range(n):
+                if r-1 >= 0:
+                    dp[r][c] += dp[r-1][c]
+                if c-1 >= 0:
+                    dp[r][c] += dp[r][c-1]
         
-        def dfs(r, c):
-            if r < 0 or c < 0:
-                return 0
-            if dp[r][c] != -1:
-                return dp[r][c]
-            dp[r][c] = dfs(r-1,c) + dfs(r,c-1)
-            return dp[r][c]
-
-        return dfs(m-1, n-1)
-
+        return dp[m-1][n-1]
             
 
 
