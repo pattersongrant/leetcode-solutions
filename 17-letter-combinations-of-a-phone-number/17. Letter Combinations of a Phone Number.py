4051f7a45ad9f833245ad020cbfef8dfb2d9a138
Grant Patterson <grantpa@umich.edu>
2026-03-06 18:17:37 -0500

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (74.13%) - Gitcode

--- 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
+++ 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
@@ -1,24 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        '''
+        I am given a string of digits from 2-9
+        I will create a hashmap that maps each digit to a string of the corresponding possible letters
+        
+
+        using a backtracking solution, I will recursively go through each digit in the input string and return when the length of the output matches the length of the input string
+        '''
+
+        digToNum = {"2" : "abc", "3": "def", "4" : "ghi", "5" : "jkl", 
+                    "6" : "mno", "7": "pqrs", "8" : "tuv", "9" : "wxyz"}
+        
         res = []
-        digit_to_letters = {2 : "abc", 3 : "def", 4: "ghi", 5:"jkl", 6:"mno", 7:"pqrs", 8:"tuv", 9:"wxyz"}
-
-        if not digits:
-            return []
-
-        def dfs(i, cur):
-            if len(cur) == len(digits):
+        def backtrack(i, cur): #i is index of input/output string, cur is array of letters
+            if i == len(digits):
                 res.append(cur)
-
-            if i >= len(digits):
                 return
-
-            for c in digit_to_letters[int(digits[i])]:
-                dfs(i+1, cur + c)
-
-        dfs(0, "")
+            
+            for c in digToNum[digits[i]]:
+                backtrack(i+1, cur + c)
+            
+        backtrack(0, "")
 
         return res
+            
+            
+
             
 
         
