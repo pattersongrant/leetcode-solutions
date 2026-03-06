df9eeba3c0d7d50f4425076852fb2fa17e8a8571
Grant Patterson <grantpa@umich.edu>
2026-03-06 18:24:50 -0500

    Runtime: 3 ms (20.25%), Memory: 19.6 MB (14.80%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -1,27 +1,34 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ROWS, COLS = m, n
-        
-        memo = []
+        '''
+        I am going to do a top-down DP solution starting from the finish line
 
-        for i in range(m):
-            memo.append([-1 for i in range(n)])
-        memo[0][0] = 1
+        the recurrence relation for the number of paths at any given point is the number of paths to get to the square above + the number of paths to get to the square to the left
 
-        def numWays(r, c):
-            if memo[r][c] != -1:
-                return memo[r][c]
+        So I will start from the finish and the by using a top down recursion function will work from the base case all the way to the start.
 
-            res = 0
-            if r - 1 >= 0:
-                res += (numWays(r-1, c))
-            if c - 1 >= 0:
-                res += (numWays(r, c-1))
+        I will also use a memo the size of m * n to ensure that I cache any already calculated squares
+        '''
+        dp = [[-1] * n for i in range(m)]
+        dp[0][0] = 1
+        # dp[1][0] = 1
+        # dp[0][1] = 1
+        def dfs(r, c):
+            if r < 0 or c < 0:
+                return 0
+            if dp[r][c] != -1:
+                return dp[r][c]
+            dp[r][c] = dfs(r-1,c) + dfs(r,c-1)
+            return dp[r][c]
+
+        return dfs(m-1, n-1)
+
             
-            memo[r][c] = res
 
-            return res
+
+
+
+            
+            
+
         
-
-        return numWays(ROWS-1, COLS-1)
-
