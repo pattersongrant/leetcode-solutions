2f5e810f08be0f0b72ae30ada56e118f61e70c86
Grant Patterson <grantpa@umich.edu>
2026-03-08 00:17:16 -0500

    Runtime: 7 ms (55.62%), Memory: 22.7 MB (31.02%) - Gitcode

--- 130-surrounded-regions/130. Surrounded Regions.py
+++ 130-surrounded-regions/130. Surrounded Regions.py
@@ -1,39 +1,37 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         ROWS, COLS = len(board), len(board[0])
 
-        #turn all unsurrounded O's into T's (connected to a edge). 
-        #Change all other O's to X's, then change T's back to O's.
+        noCap = set()
 
-        def capture(r, c): #only run on cells connected to an edge
-            if (r < 0 or c < 0 or r == ROWS or 
-            c == COLS or board[r][c] != "O"):
+        def dfs(r,c):
+            if (r,c) in noCap:
                 return
-            
-            board[r][c] = "T"
-            capture(r+1, c)
-            capture(r-1, c)
-            capture(r, c+1)
-            capture(r, c-1)
-            
+            noCap.add((r,c))
+            if r+1 < ROWS and board[r+1][c] == 'O':
+                dfs(r+1, c)
+            if c + 1 < COLS and board[r][c+1] == 'O':
+                dfs(r, c+1)
+            if r-1 >= 0 and board[r-1][c] == 'O':
+                dfs(r-1, c)
+            if c - 1 >= 0 and board[r][c-1] == 'O':
+                dfs(r, c-1)
+
         for r in range(ROWS):
             for c in range(COLS):
-                if (board[r][c] == "O" and (r == 0 or r == ROWS - 1
-                or c == 0 or c == COLS - 1)):
-                    capture(r,c)
+                if ((not (r==0 or r == ROWS - 1 or c == 0 or c == COLS -1)) 
+                    or (r,c) in noCap):
+                    continue
+                if board[r][c] == 'O':
+                    dfs(r,c)
         
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] ="X"
+                if board[r][c] == 'O' and (r,c) not in noCap:
+                    board[r][c] = 'X'
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "T":
-                    board[r][c] = "O"
         
-
-
-
-
-            
+        
