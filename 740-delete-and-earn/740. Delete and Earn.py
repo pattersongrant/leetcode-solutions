d9575ef560857bd76f7c74ebf2c5f0e02f654866
Grant Patterson <grantpa@umich.edu>
2026-03-08 16:24:27 -0400

    Runtime: 15 ms (20.76%), Memory: 21 MB (63.11%) - Gitcode

--- 740-delete-and-earn/740. Delete and Earn.py
+++ 740-delete-and-earn/740. Delete and Earn.py
@@ -1,9 +1,11 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
             counts = defaultdict(int)
+            maxNum = 0
             for n in nums:
+                maxNum = max(maxNum, n)
                 counts[n] += n
-            for i in range(max(nums)+1):
+            for i in range(maxNum+1):
                 minusOne = 0
                 if i - 1 in counts:
                     minusOne = counts[i-1]
@@ -13,4 +15,4 @@ class Solution:
 
                 counts[i] = max(minusOne, counts[i] + minusTwo)
         
-            return counts[max(nums)]
+            return counts[maxNum]
