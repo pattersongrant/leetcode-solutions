b71e3e006b42ae13b21f16e2535ce1baa9c2b633
Grant Patterson <grantpa@umich.edu>
2026-03-08 01:14:44 -0500

    Runtime: 4 ms (32.93%), Memory: 21.2 MB (46.75%) - Gitcode

--- 419-battleships-in-a-board/419. Battleships in a Board.py
+++ 419-battleships-in-a-board/419. Battleships in a Board.py
@@ -2,22 +2,11 @@ class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         ROWS, COLS = len(board), len(board[0])
 
-        def dfs(r, c):
-            board[r][c] = '.'
-            if r+1 < ROWS and board[r+1][c] == 'X':
-                dfs(r+1, c)
-            if c+1 < COLS and board[r][c+1] == 'X':
-                dfs(r, c+1)
-            if r-1 >= 0 and board[r-1][c] == 'X':
-                dfs(r-1, c)
-            if c-1 >= 0 and board[r][c-1] == 'X':
-                dfs(r, c-1)
         res = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == 'X':
+                if board[r][c] == 'X' and (r-1 < 0 or board[r-1][c] != 'X') and (c-1 < 0 or board[r][c-1] != 'X'):
                     res += 1
-                    dfs(r,c)
         return res
 
 
