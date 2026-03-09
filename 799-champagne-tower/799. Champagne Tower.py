1faea82d7554a3926368252e982544e481361e66
Grant Patterson <grantpa@umich.edu>
2026-03-08 23:10:31 -0400

    Runtime: 153 ms (5.19%), Memory: 43.4 MB (5.01%) - Gitcode

---
+++ 799-champagne-tower/799. Champagne Tower.py
@@ -0,0 +1,34 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        '''
+        take from up and left and up and right. You get half the excess there
+        if not up and left or not up and right, you get 0 from there.
+        '''
+        dp = [([0.0] * (query_row+1)) for r in range(query_row+1)]
+        dp[0][0] = poured
+        seen = set()
+        seen.add((0,0))
+        def dfs(row, idx):
+            amount = 0
+            if row < 0 or row >= len(dp) or idx < 0 or idx >= (query_row + 1):
+                return 0
+            if (row, idx) in seen:
+                return dp[row][idx]
+            seen.add((row, idx))
+            
+            above1 = dfs(row-1, idx)
+            above2 = dfs(row-1, idx-1)
+            if above1 > 1:
+                amount += (above1-1) / 2.0
+            if above2 > 1:
+                amount += (above2-1) / 2.0 
+
+            dp[row][idx] = amount
+            return dp[row][idx]
+        res = dfs(query_row, query_glass)
+        return res if res < 1 else 1.0 
+            
+            
+
+
+
