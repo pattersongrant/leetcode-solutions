fb50fc6f8634f48ca32c13e3db5402511fe8782c
Grant Patterson <grantpa@umich.edu>
2026-03-09 00:21:06 -0400

    Runtime: 1043 ms (11.44%), Memory: 20.1 MB (34.87%) - Gitcode

--- 322-coin-change/322. Coin Change.py
+++ 322-coin-change/322. Coin Change.py
@@ -2,13 +2,15 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = {}
         dp[0] = 0
-        for i in range(1, amount+1):
-            dp[i] = 2**31           
+        for i in range(1, amount+1):       
             for c in coins:
-                if i - c < 0 or dp[i - c] == 2**31: 
+                if i - c < 0: 
                     continue
-                dp[i] = min(dp[i], dp[i-c] + 1)
-        if dp[amount] == 2 **31:
+                if i - c in dp:
+                    if i not in dp:
+                        dp[i] = float('inf')
+                    dp[i] = min(dp[i], dp[i-c] + 1)
+        if amount not in dp:
             return -1
         return dp[amount]
 
