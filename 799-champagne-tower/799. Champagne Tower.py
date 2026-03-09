2bdb55bb84291541cd6b77a0bbf7be8973463066
Grant Patterson <grantpa@umich.edu>
2026-03-08 23:38:21 -0400

    Runtime: 171 ms (5.08%), Memory: 19.5 MB (31.50%) - Gitcode

--- 799-champagne-tower/799. Champagne Tower.py
+++ 799-champagne-tower/799. Champagne Tower.py
@@ -4,27 +4,17 @@ class Solution:
         take from up and left and up and right. You get half the excess there
         if not up and left or not up and right, you get 0 from there.
         '''
-        dp = [([-1] * (r+1)) for r in range(query_row+1)]
+        dp = [([0] * (r+1)) for r in range(query_row+1)]
         dp[0][0] = poured
-        def dfs(row, idx):
-            amount = 0
-            if row < 0 or row >= len(dp) or idx < 0 or idx >= (len(dp[row])):
-                return 0
-            if dp[row][idx] != -1:
-                return dp[row][idx]
-            dp[row][idx] = 0
-            
-            above1 = dfs(row-1, idx)
-            above2 = dfs(row-1, idx-1)
-            if above1 > 1:
-                amount += (above1-1) / 2.0
-            if above2 > 1:
-                amount += (above2-1) / 2.0 
 
-            dp[row][idx] = amount
-            return dp[row][idx]
-        res = dfs(query_row, query_glass)
-        return res if res < 1 else 1.0 
+        for r in range(len(dp)):
+            for c in range(len(dp[r])):
+                if r - 1 >= 0 and c in range(len(dp[r-1])) and dp[r-1][c] > 1:
+                    dp[r][c] += (dp[r-1][c] - 1) / 2
+                if r - 1 >= 0 and c-1 in range(len(dp[r-1])) and dp[r-1][c-1] > 1:
+                    dp[r][c] += (dp[r-1][c-1] - 1) / 2
+                
+        return dp[query_row][query_glass] if dp[query_row][query_glass] < 1 else 1
             
             
 
