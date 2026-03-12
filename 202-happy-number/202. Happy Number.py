1a716fedc33db67f106b13d3f849fe81aafeeb63
Grant Patterson <grantpa@umich.edu>
2026-03-12 11:27:34 -0400

    Runtime: 3 ms (37.22%), Memory: 19.3 MB (86.22%) - Gitcode

--- 202-happy-number/202. Happy Number.py
+++ 202-happy-number/202. Happy Number.py
@@ -1,17 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        nStr = str(n)
+        
         seen = set()
-        while nStr != "" and nStr != "1":
-            res = 0
-            for i in range(len(nStr)):
-                intv = int(nStr[i:i+1])
-                res += (intv ** 2)
-                print(res)
-            if res in seen:
+        while n != 1:
+            if n in seen:
                 return False
-            seen.add(res)
-            nStr = str(res)
-            print(nStr)
-
-        return nStr == "1"
+            seen.add(n)
+            new = 0
+            for c in str(n):
+                new += int(c) ** 2
+            n = new
+            
+        return True
