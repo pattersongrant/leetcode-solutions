273a7d1764426c0e23d6447d89a701550f9dfd87
Grant Patterson <grantpa@umich.edu>
2026-05-25 15:10:25 -0700

    Runtime: 353 ms (20.18%), Memory: 72.1 MB (8.10%) - Gitcode

---
+++ 1851-minimum-interval-to-include-each-query/1851. Minimum Interval to Include Each Query.py
@@ -0,0 +1,52 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        #keep a minHeap w/ (length, right[i])
+        #keep a hashmap mapping queries to positons
+        queryToResIdxs = defaultdict(list)
+        for i,q in enumerate(queries):
+            queryToResIdxs[q].append(i)
+        res = [-1] * len(queries)
+        queries.sort()
+        minHeap = []
+        intervals.sort()
+        curInt = 0
+        '''
+        only add interval to minHeap, if the start index is less than the query
+        add all the intervals until the start index > query
+
+        when adding to minHeap, pop until there is an interval where the right[i] is less       than query
+
+        '''
+        for query in queries:
+            # add all valid start idx intervals
+            while curInt != len(intervals) and intervals[curInt][0] <= query:
+                i = intervals[curInt]
+                heapq.heappush(minHeap, (i[1]-i[0]+1, i[1]))
+                curInt += 1
+
+            # pop minHeaps that end too early
+            while minHeap and minHeap[0][1] < query:
+                heapq.heappop(minHeap)
+            if minHeap:
+                for idx in queryToResIdxs[query]:
+                    res[idx] = minHeap[0][0]
+            else:
+                for idx in queryToResIdxs[query]:
+                    res[idx] = -1
+        
+        return res
+            
+
+            
+            
+
+
+            
+
+                
+
+
+
+
+
+
