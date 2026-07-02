ef7ea7a7c1d6057e7124ed9f72d95365319fa3e7
Grant Patterson <grantpa@umich.edu>
2026-07-02 15:59:39 -0700

    Runtime: 1079 ms (29.73%), Memory: 68.4 MB (52.78%) - Gitcode

---
+++ 211-design-add-and-search-words-data-structure/211. Design Add and Search Words Data Structure.py
@@ -0,0 +1,50 @@
+'''
+if there is a dot in search word - try every letter in children at that point until match or no more
+'''
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+
+    def search(self, word: str) -> bool:
+        
+        def searchFromHere(start, remaining):
+            cur = start
+            for i, c in enumerate(remaining):
+                if c not in cur.children:
+                    if c == ".":
+                        for next_c in cur.children:
+                            if searchFromHere(cur.children[next_c], remaining[i+1::]):
+                                return True
+                    return False
+                cur = cur.children[c]
+            return cur.endOfWord
+
+
+
+        return searchFromHere(self.root, word)
+
+        
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
