e9fe168d31130cf2991a4f52f5b3ece4f78e1f1f
Grant Patterson <grantpa@umich.edu>
2026-07-08 17:17:32 -0700

    Runtime: 15 ms (14.68%), Memory: 24.2 MB (11.91%) - Gitcode

---
+++ 124-binary-tree-maximum-path-sum/124. Binary Tree Maximum Path Sum.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        '''
+        if a path is negative you don't want to include it
+        you can travel 
+        for any node:
+            res can be any sub part or include left and right, but the returned part needs to include the current node and can only pick one side
+            res = max(leftsum, rightsum, rightsum + node, leftsum + node, node, leftsum + rightsum + node, res)
+            return max(leftsum + node, rightsum + node, node)
+        '''
+
+        self.res = root.val
+        def travel(node):
+            val, leftsum, rightsum = node.val, -float('inf'), -float('inf')
+            if not node.left and not node.right:
+                return val
+            if node.left:
+                leftsum = travel(node.left)
+            if node.right:
+                rightsum = travel(node.right)
+            self.res = max(leftsum, rightsum, rightsum + val, leftsum + val, leftsum + rightsum + val, val, self.res)
+            return max(leftsum + val, rightsum + val, val)
+        travel(root)
+        return self.res
+
+
+
+
+
+
+
+
+
+
+
+
+        
