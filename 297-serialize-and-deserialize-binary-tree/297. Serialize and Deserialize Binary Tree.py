ddadc571bbaad104cbd8989e3a687e468a5456eb
Grant Patterson <grantpa@umich.edu>
2026-07-11 20:27:26 -0700

    Runtime: 93 ms (37.08%), Memory: 22.2 MB (95.88%) - Gitcode

--- 297-serialize-and-deserialize-binary-tree/297. Serialize and Deserialize Binary Tree.py
+++ 297-serialize-and-deserialize-binary-tree/297. Serialize and Deserialize Binary Tree.py
@@ -1,60 +1,52 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from collections import deque
 
 class Codec:
 
     def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
-        self.res = []
+        if not root:
+            return ""
 
-        def dfs(node):
-            if not node:
-                self.res.append("N")
-                return
-            self.res.append(str(node.val))
-            
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
-        return ",".join(self.res)
-        
+        q = deque([root])
+        res = []
+
+        while q:
+            node = q.popleft()
+
+            if node:
+                res.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                res.append("N")
+
+        # Optional: remove trailing nulls
+        while res and res[-1] == "N":
+            res.pop()
+
+        return ":".join(res)
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-        vals = data.split(",")
-        self.i = 0
+        if not data:
+            return None
 
-        def dfs():
-            if vals[self.i] == "N":
-                self.i += 1
-                return 
+        vals = data.split(":")
+        root = TreeNode(int(vals[0]))
+        q = deque([root])
 
-            node = TreeNode(vals[self.i])
-            self.i += 1
-            node.left = dfs()
-            node.right = dfs()
+        i = 1
+        while q and i < len(vals):
+            node = q.popleft()
 
-            return node
+            # left child
+            if i < len(vals) and vals[i] != "N":
+                node.left = TreeNode(int(vals[i]))
+                q.append(node.left)
+            i += 1
 
-        return dfs()
+            # right child
+            if i < len(vals) and vals[i] != "N":
+                node.right = TreeNode(int(vals[i]))
+                q.append(node.right)
+            i += 1
 
-
-
-        
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
+        return root
