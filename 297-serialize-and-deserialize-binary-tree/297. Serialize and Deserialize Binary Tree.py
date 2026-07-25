c78622e1f8f17ae95e4d03c89ac6fd924916a5b1
Grant Patterson <grantpa@umich.edu>
2026-07-24 22:34:42 -0700

    Runtime: 85 ms (76.22%), Memory: 22.3 MB (86.79%) - Gitcode

--- 297-serialize-and-deserialize-binary-tree/297. Serialize and Deserialize Binary Tree.py
+++ 297-serialize-and-deserialize-binary-tree/297. Serialize and Deserialize Binary Tree.py
@@ -1,52 +1,50 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Codec:
-
-    def serialize(self, root):
-        if not root:
-            return ""
-
-        q = deque([root])
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        q = deque()
+        q.append(root)
         res = []
-
         while q:
             node = q.popleft()
-
             if node:
                 res.append(str(node.val))
                 q.append(node.left)
                 q.append(node.right)
             else:
                 res.append("N")
-
-        # Optional: remove trailing nulls
         while res and res[-1] == "N":
             res.pop()
+        return ",".join(res)
 
-        return ":".join(res)
-
-    def deserialize(self, data):
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
         if not data:
             return None
-
-        vals = data.split(":")
+        vals = data.split(",")
         root = TreeNode(int(vals[0]))
         q = deque([root])
 
         i = 1
+
         while q and i < len(vals):
             node = q.popleft()
 
-            # left child
             if i < len(vals) and vals[i] != "N":
                 node.left = TreeNode(int(vals[i]))
                 q.append(node.left)
             i += 1
-
-            # right child
             if i < len(vals) and vals[i] != "N":
                 node.right = TreeNode(int(vals[i]))
                 q.append(node.right)
             i += 1
-
         return root
+        
