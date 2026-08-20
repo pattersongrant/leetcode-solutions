04ee32c068a742e407138a361640ae2673b6d82b
Grant Patterson <grantpa@umich.edu>
2026-08-20 09:15:51 -0400

    Runtime: 137 ms (49.82%), Memory: 22 MB (69.02%) - Gitcode

--- 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
@@ -1,8 +1,7 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         ROWS, COLS = len(matrix), len(matrix[0])
-        for r in range(ROWS):
-            print(matrix[r])
+
         dp = [[-1] * COLS for _ in range(ROWS)]
         # dp[r][c] : distance to lowest value starting from this cell
         res = 1
@@ -10,11 +9,8 @@ class Solution:
             #find the bottom and update res, while updating dp going back up
             nonlocal res
             res = max(res, traveled)
-            # print((r,c), matrix[r][c], traveled, res)
             if dp[r][c] != -1:
                 res = max(res, traveled + dp[r][c])
-                # print(matrix[r][c],"returning dp", dp[r][c])
-
                 return dp[r][c]
 
             up, down, left, right = -1, -1, -1, -1
@@ -35,15 +31,9 @@ class Solution:
                 right = search(r, c+1, traveled + 1, (r,c))
             
             maxSearch = max(up,down,left,right)
-            # if matrix[r][c] == 44:
-            #     print(up,down,left,right)
             dp[r][c] = maxSearch + 1
-            # print("just cached", matrix[r][c], "as", maxSearch+1)
-            # if dp[r][c] == 0:
-            #     print("minimum hit", matrix[r][c])
 
             return dp[r][c]
-#'''94, 64, 48, 44, 38, 36, 32, 26, 6'''
         for r in range(ROWS):
             for c in range(COLS):
                 #search only starting from local maximums (>=)
