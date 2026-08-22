ab30fafd674946ca06427d25c711b4eb4a247e63
Grant Patterson <grantpa@umich.edu>
2026-08-22 13:31:49 -0400

    Runtime: 868 ms (10.99%), Memory: 232.1 MB (19.32%) - Gitcode

--- 115-distinct-subsequences/115. Distinct Subsequences.py
+++ 115-distinct-subsequences/115. Distinct Subsequences.py
@@ -1,6 +1,10 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        cache = defaultdict(int)
+        #before i was caching whether something could eventually form t
+        #this is caching the actual number of possibilities starting at that point
+
+        #Cache # of ways, not just yes/no if possible
+        cache = {}
         def dfs(i,j):
             if (i,j) in cache:
                 return cache[(i,j)]
@@ -9,8 +13,10 @@ class Solution:
             if i == len(s):
                 return 0
             if s[i] == t[j]:
-                cache[(i,j)] += dfs(i+1,j+1)
-            cache[(i,j)] += dfs(i+1, j)
-            return cache[(i,j)]
+                cache[(i,j)] = dfs(i+1,j+1) + dfs(i+1, j)
+                return cache[(i,j)]
+            else:
+                cache[(i,j)] = dfs(i+1, j)
+                return cache[(i,j)]
         
         return dfs(0,0)
