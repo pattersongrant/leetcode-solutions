c22a019490ef84d186cbb92c6b083c803f48efcc
Grant Patterson <grantpa@umich.edu>
2026-08-26 07:39:39 -0400

    Runtime: 377 ms (8.56%), Memory: 21.9 MB (13.07%) - Gitcode

---
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -0,0 +1,56 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        '''
+        network of directed nodes (times)
+        each edge has a weight (travel time), >= 0
+        k = first node sending a signal from
+
+        return either the minimum time to reach all nodes
+        OR -1 if impossible
+
+        My solution: use Prim's, as we're starting from a specific node and trying to build an MST.
+        
+        idea: build a map of neighbors
+
+        start w/ k, adding it and it's neighbors to a min heap sorted by weight.
+        then keep going, adding the lowest edge (if unseen so far)  and the neighbors connected to the newly connected node 
+        '''
+
+        minHeap = []
+        neighbors = defaultdict(list) #node : list of (neighbor, edge)
+        for t in times:
+            node, target, time = t[0], t[1], t[2]
+            neighbors[node].append((target, time))
+        
+        for neighbor, edge in neighbors[k]:
+            heapq.heappush(minHeap, (edge, neighbor, k))
+
+        toGetTo = {}
+        toGetTo[k] = 0
+
+
+        
+        res = 0
+        while minHeap:
+            edge, neighbor, cameFrom = heapq.heappop(minHeap)
+            #if already visited w/ a smaller weight, skip it
+            if neighbor in toGetTo:
+                continue
+
+            
+            #we've seen this new node now
+            toGetTo[neighbor] = edge
+            res = max(toGetTo[neighbor], res)
+
+            #add all edges of newly added node
+            for n1, e in neighbors[neighbor]:
+                heapq.heappush(minHeap, (toGetTo[neighbor] + e, n1, neighbor))
+
+        if not len(toGetTo) == n:
+            return -1
+
+        return res
+
+
+
+        
