cd0051b81dbd51649ba088ccc438c8fe0e154fed
Grant Patterson <grantpa@umich.edu>
2026-08-29 07:19:52 -0400

    Runtime: 29 ms (44.74%), Memory: 19.9 MB (62.70%) - Gitcode

--- 778-swim-in-rising-water/778. Swim in Rising Water.py
+++ 778-swim-in-rising-water/778. Swim in Rising Water.py
@@ -1,11 +1,6 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # start at 0,0 - how long to get to bottom right ?
-        # you can swim horizontally or vertically, if grid[r][c] <= t
-        # use dijkstra's where elevation is the weight
-        # in minHeap: (maxToGetHere, r, c)
         ROWS, COLS = len(grid), len(grid[0])
-
 
         minH = [(grid[0][0], 0, 0)]
         directions = ((0,1), (1,0), (-1,0), (0,-1))
@@ -18,8 +13,6 @@ class Solution:
             visit.add((r,c))
             if (r,c) == (ROWS-1, COLS-1):
                 return maxToGetHere
-            
-            
 
             for newR, newC in directions:
                 newR, newC = r + newR, c + newC
