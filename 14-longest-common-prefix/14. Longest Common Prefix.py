ec2a9ca1dd0a6392a50486dfc1c7e9ba0eb1b9d2
Grant Patterson <grantpa@umich.edu>
2026-08-30 15:50:55 -0400

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (31.65%) - Gitcode

--- 14-longest-common-prefix/14. Longest Common Prefix.py
+++ 14-longest-common-prefix/14. Longest Common Prefix.py
@@ -1,5 +1,30 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        found = True
+        prevc = None
+        res = []
+        i = 0
+        while found:
+            for s in strs:
+                if i == len(s):
+                    found = False
+                    break
+                c = s[i]
+                if not prevc:
+                    prevc = c            
+                if c != prevc:
+                    found = False
+                    break
+                prevc = c
+            if found:
+                res.append(prevc)
+            i += 1
+            prevc = None
+        return "".join(res)
+
+
+
+
         prefixes = defaultdict(int)
         
         res = ""
