209f449dfeaf9738ddde1c9b5ed0123556b68c50
Grant Patterson <grantpa@umich.edu>
2026-09-01 21:34:24 -0400

    Runtime: 22 ms (34.30%), Memory: 20.2 MB (7.82%) - Gitcode

--- 1047-remove-all-adjacent-duplicates-in-string/1047. Remove All Adjacent Duplicates In String.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047. Remove All Adjacent Duplicates In String.py
@@ -1,23 +1,15 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
         
-        i = 1
         arr = []
+
         for c in s:
-            arr.append(c)
-
-        while i < len(arr):
-            if arr[i-1] == arr[i]:
-                arr.pop(i)
-                arr.pop(i-1)
-                if i != 1:
-                    i -= 1
+            if arr and arr[-1] == c:
+                arr.pop()
             else:
-                i += 1
-
-
+                arr.append(c)
+        
         return "".join(arr)
-
 
 
 
