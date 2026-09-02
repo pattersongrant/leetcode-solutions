f1db188dd53d0f074f8553155446e59727123b25
Grant Patterson <grantpa@umich.edu>
2026-09-01 21:34:40 -0400

    Runtime: 3 ms (82.56%), Memory: 14.4 MB (34.88%) - Gitcode

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st; 
+        
+        for (int i = 0; i < s.size(); ++i) { 
+            if (st.empty()) { 
+                st.push(s[i]);
+                continue; 
+            }
+            
+            if (s[i] == st.top()) { 
+                st.pop(); 
+            }
+            else { 
+                st.push(s[i]); 
+            }
+        }
+         string ans;
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+    
+        reverse(ans.begin(), ans.end()); // Need to reverse!
+        return ans;
+    }
+};
