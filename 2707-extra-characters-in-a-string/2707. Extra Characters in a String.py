9de913ef259f2cec5c72da370dc8f250e58d96b9
Grant Patterson <grantpa@umich.edu>
2026-09-23 22:52:18 -0400

    Runtime: 100 ms (44.57%), Memory: 24.1 MB (23.67%) - Gitcode

--- 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
+++ 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
@@ -1,46 +1,24 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-
-        '''
-        dfs solution.
-        turn dictionary into a set
-
-        either take or skip every letter.
-        skip = extra character
-
-        and then if there is a chance to take a word, then take or skip every word
-
-
-        return the time that you got to the end and had minimum extra characters
-        '''
-
         dictionary = set(dictionary)
         res = len(s)
-        seen = set()
+        dp = {len(s) : 0}
 
-        def dfs(i, curBuild, extraCharacters):
-            #each iteration, you can add the letter to your curBuild or reset
-            #extraCharacters stays the same unless you find a word, then you can subtract the length of that word
+        def dfs(i):
             nonlocal res
-            # print((i,curBuild,extraCharacters))
-            if (i,curBuild,extraCharacters) in seen:
-                return
-            seen.add((i,curBuild,extraCharacters))            
-            if i == len(s):
-                if curBuild in dictionary:
-                    extraCharacters -= len(curBuild)
-                res = min(res, extraCharacters)
-                return
-            let = s[i]
-            inDict = True if curBuild in dictionary else False
+            if i in dp:
+                return dp[i]
 
-            if inDict:
-                dfs(i+1, let, extraCharacters - len(curBuild)) #reset curBuild
-                dfs(i+1, curBuild+let, extraCharacters)
-            else:
-                dfs(i+1, curBuild+let, extraCharacters) #skip word / addToCurBuild
-                dfs(i+1, let, extraCharacters) #reset curBuild
+            if i == len(s):
+                return 0
+
+            res = 1 + dfs(i+1)
+            
+            for j in range(i, len(s)):
+                if s[i:j+1] in dictionary:
+                    res = min(res, dfs(j+1))
+            dp[i] = res
+            return dp[i]
         
-        dfs(0, "", len(s))
-        return res
+        return dfs(0)
         
