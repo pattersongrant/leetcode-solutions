3879302bb7a5e630a548580b33badb950b8e8832
Grant Patterson <grantpa@umich.edu>
2026-09-24 06:38:38 -0400

    Runtime: 19 ms (34.57%), Memory: 23.1 MB (14.52%) - Gitcode

--- 997-find-the-town-judge/997. Find the Town Judge.py
+++ 997-find-the-town-judge/997. Find the Town Judge.py
@@ -1,11 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        truststhisperson = defaultdict(list)
-        whotheytrust = defaultdict(list)
+        truststhisperson = defaultdict(set)
+        whotheytrust = defaultdict(set)
 
         for src, dst in trust:
-            whotheytrust[src].append(dst)
-            truststhisperson[dst].append(src)
+            whotheytrust[src].add(dst)
+            truststhisperson[dst].add(src)
 
         for person in range(1,n+1):
             if len(whotheytrust[person]) == 0 and len(truststhisperson[person]) == n-1:
