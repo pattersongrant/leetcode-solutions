a025b635dab866a07714abdb7fd80d91dc909111
Grant Patterson <grantpa@umich.edu>
2026-09-23 22:53:03 -0400

    Runtime: 92 ms (54.95%), Memory: 23.7 MB (30.19%) - Gitcode

--- 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
+++ 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
@@ -1,17 +1,11 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         dictionary = set(dictionary)
-        res = len(s)
         dp = {len(s) : 0}
 
         def dfs(i):
-            nonlocal res
             if i in dp:
                 return dp[i]
-
-            if i == len(s):
-                return 0
-
             res = 1 + dfs(i+1)
             
             for j in range(i, len(s)):
